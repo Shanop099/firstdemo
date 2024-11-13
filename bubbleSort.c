527f7e666857e53f0fc3c85f3c77b334dd7ed7c8
Shanop099 <guptaishan0660@gmail.com>
2024-11-13 14:07:42 +0530

    reset array

--- bubbleSort.c
+++ bubbleSort.c
@@ -20,7 +20,7 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11};
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     bubbleSort(arr, n);
