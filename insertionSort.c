2513b19f43c1c5ba2ef840ab1d388334e6a210f9
Shanop099 <guptaishan0660@gmail.com>
2024-11-13 14:09:34 +0530

    changed size

--- insertionSort.c
+++ insertionSort.c
@@ -21,7 +21,7 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {12, 11, 13, 5, 6};
+    int arr[] = {12, 11, 13, 5, 6 ,90};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     insertionSort(arr, n);
